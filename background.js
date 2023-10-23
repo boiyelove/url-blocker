242ed0d9ecfa515f4c5cb35628a1b0876eb1690a
Damilola Afolabi <daahrmmieboiye@gmail.com>
2023-10-23 09:11:20 +0100

    Added fegz tiktok urls

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 // Define the URLs to block
-var urlsToBlock = [  "https://www.instagram.com/miss_fegz/",  "https://www.instagram.com/nextupwithfegz/",  "https://www.instagram.com/koria.brand/", "https://www.instagram.com/being.fitfegz/"];
+var urlsToBlock = [
+  "https://www.instagram.com/miss_fegz/",
+  "https://www.instagram.com/nextupwithfegz/",
+  "https://www.instagram.com/koria.brand/",
+  "https://www.instagram.com/being.fitfegz/",
+  "https://www.tiktok.com/@beingfitfegz/video/7292715046428413190",
+  "https://www.tiktok.com/@wakawakafegz",
+  "https://www.instagram.com/koriaofficial/"
+];
 
 // Listen for web requests and block URLs that match
 chrome.webRequest.onBeforeRequest.addListener(
