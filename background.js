b9847d9a14c50b25c96529ed9cf8d4bf8d965f5a
boiyelove <daahrmmieboiye@gmail.com>
2023-10-26 09:27:55 +0100

    upgraded to keywords based page blocking

--- background.js
+++ background.js
@@ -1,21 +1,48 @@
-// Define the URLs to block
-var urlsToBlock = [
-  "https://www.instagram.com/miss_fegz/",
-  "https://www.instagram.com/nextupwithfegz/",
-  "https://www.instagram.com/koria.brand/",
-  "https://www.instagram.com/being.fitfegz/",
-  "https://www.tiktok.com/@beingfitfegz/video/7292715046428413190",
-  "https://www.tiktok.com/@wakawakafegz",
-  "https://www.instagram.com/koriaofficial/"
-];
+// Define the keywords to match
+var keywordsToMatch = ["miss_fegz","nextupwithfegz","koria.brand", "fegz","being.fitfegz", "fega", "koria"];
+
+
+// Function to check if a URL, page title, or AJAX request contains keywords
+function containsKeywords(details) {
+  var url = details.url || "";
+  var pageTitle = details.title || "";
+  var requestData = details.requestBody ? details.requestBody.formData : {};
+
+  // Check if URL, page title, or request data contains keywords
+  return (
+    keywordsToMatch.some(keyword => url.includes(keyword)) ||
+    keywordsToMatch.some(keyword => pageTitle.includes(keyword)) ||
+    Object.values(requestData).some(value =>
+      keywordsToMatch.some(keyword => value.includes(keyword))
+    )
+  );
+}
 
 // Listen for web requests and block URLs that match
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    if (urlsToBlock.includes(details.url)) {
-      return {cancel: true};
+    if (containsKeywords(details)) {
+      return { cancel: true };
     }
   },
-  {urls: ["<all_urls>"]},
+  { urls: ["<all_urls>"] },
   ["blocking"]
+);
+
+// Listen for changes in the URL for SPAs
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  if (containsKeywords(details)) {
+    chrome.tabs.update(details.tabId, { url: "about:blank" });
+  }
+});
+
+// Listen for completed AJAX requests
+chrome.webRequest.onCompleted.addListener(
+  function(details) {
+    if (containsKeywords(details)) {
+      // Handle the completed AJAX request as needed
+      console.log("Blocked AJAX request:", details);
+    }
+  },
+  { urls: ["<all_urls>"] }
 );
